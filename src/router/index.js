d4112503842e787683cfba583e7fbe79f9a44a94
Benjapol Worakan <benwrk@live.com>
2019-01-21 03:26:42 +0700

    Remove unused forms

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import SubmissionForm from '@/components/Forms/Submit'
-import RegistrationForm from '@/components/Forms/Register'
-import Thankyou from '@/components/Forms/Thankyou'
-import FAQForm from '@/components/Forms/Question'
 import Home from '@/components/Home/Home'
 
 Vue.use(Router)
@@ -13,78 +9,6 @@ export default new Router({
     {
       path: '/',
       component: Home
-    },
-    {
-      path: '/key-dates',
-      component: Home
-    },
-    {
-      path: '/announcements',
-      component: Home
-    },
-    {
-      path: '/committee',
-      component: Home
-    },
-    {
-      path: '/program',
-      component: Home
-    },
-    {
-      path: '/speakers',
-      component: Home
-    },
-    {
-      path: '/accomodation',
-      component: Home
-    },
-    {
-      path: '/registration',
-      component: Home
-    },
-    {
-      path: '/submission',
-      component: Home
-    },
-    {
-      path: '/venue',
-      component: Home
-    },
-    {
-      path: '/faq',
-      component: Home
-    },
-    {
-      path: '/contact',
-      component: Home
-    },
-    {
-      path: '/forms/registration',
-      component: RegistrationForm,
-      beforeEnter (to, from, next) {
-        // Put the full page url including the protocol http(s) below
-        window.location = 'https://goo.gl/forms/tGXprIOo4ekyLJKo2'
-      }
-    },
-    {
-      path: '/forms/registration/thankyou',
-      component: Thankyou
-    },
-    {
-      path: '/forms/submission',
-      component: SubmissionForm,
-      beforeEnter (to, from, next) {
-        // Put the full page url including the protocol http(s) below
-        window.location = 'https://goo.gl/forms/qbYWPTBKITYRCMKp1'
-      }
-    },
-    {
-      path: '/forms/query',
-      component: FAQForm,
-      beforeEnter (to, from, next) {
-        // Put the full page url including the protocol http(s) below
-        window.location = 'https://goo.gl/forms/BZCI0sYmBSKCIyI33'
-      }
     }
   ],
   scrollBehavior (to, from, savedPosition) {
