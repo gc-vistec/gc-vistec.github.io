eacad9556c22b4b0686adab11d068019c0ccaf9c
Benjapol Worakan <benwrk@live.com>
2019-02-25 08:53:27 +0700

    Update training scheme

--- src/components/Home/TrainingScheme.vue
+++ src/components/Home/TrainingScheme.vue
@@ -2,23 +2,32 @@
   <section id="training-scheme" class="lead">
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
-          <h2>รูปแบบการฝึกอบรม</h2>
+        <div class="col-md-12">
+          <h2>รูปแบบการเรียนการสอน</h2>
         </div>
-        <div class="col-md-6 text-right">
-        </div>
+        <!-- <div class="col-md-6 text-right">
+        </div> -->
         <div class="col-lg-12 mx-auto lead">
-          <ul>
-            <li>
-              การฝึกอบรมจะมุ่งเน้นไปที่การปฏิบัติงานจริง และการนำความรู้ที่ได้จากห้องเรียนไปประยุกต์ใช้
-            </li>
-            <li>
-              คอร์สขนาดสั้น ซึ่งประกอบไปด้วยการเรียนการสอนทฤษฎี การลองปฏิบัติจริง และ Workshop
-            </li>
-            <li>
-              Project นำความรู้จากห้องเรียนไปใช้แก้ปัญหาธุรกิจของบริษัทโดยใช้ข้อมูลจริง
-            </li>
-          </ul>
+          <p>
+            เป็นแบบบูรณาการ เพื่อให้ผู้เรียนสามารถทำงานกับเรียนได้พร้อม ๆ กัน โดยมีความมุ่งหวังให้ผู้เรียนสามารถนำความรู้ที่ได้รับมาใช้กับการทำงานได้ทันที
+          </p>
+          <p>
+            ตั้งแต่ Level 1.1-1.3 จะใช้เวลาเรียน Level ละประมาณ 3 เดือน โดยในแต่ละ Level ผู้เรียนต้องสละเวลาทำงานไม่เกิน สัปดาห์ละ 2 วัน เพื่อร่วมกิจกรรมการเรียนการสอนดังนี้
+            <ul>
+              <li>
+                <b>Intensive Class&colon;</b> จะใช้เวลา Class ละไม่เกิน 2 วัน/สัปดาห์ อย่างน้อย 5 Class/Level จัดที่ GC ระยอง
+              </li>
+              <li>
+                <b>Data Clinic&colon;</b> เพื่อให้ผู้เรียนเข้ามาปรึกษาการทำ Project กับอาจารย์ ใช้เวลาครั้งละไม่เกิน 1 วัน/สัปดาห์ อย่างน้อย 4 ครั้ง/Level จัดที่ GC ระยอง หรือ Video Conference 
+              </li>
+              <li>
+                <b>Go &amp; See หรือ Off-site Workshop&colon;</b> จะจัดอย่างน้อย 1 ครั้ง/Level 
+              </li>
+              <li>
+                <b>ทำ Project&colon;</b> ใช้เวลาที่นอกเหนือจากการทำกิจกรรมข้างต้น
+              </li>
+            </ul>
+          </p>
           <center><img src="../../assets/training-scheme.png" alt="Training Scheme" style="max-width: 100%; width: 40rem;"></center>
         </div>
         <!-- <div class="col-md-12 text-right">
