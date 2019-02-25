2ece1d50eeea17299df662b15956486a117aaf23
Benjapol Worakan <benwrk@live.com>
2019-02-25 12:22:41 +0700

    Fix scrolling bug for in-page <a> links

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,11 @@ export default new Router({
     }
   ],
   scrollBehavior (to, from, savedPosition) {
-    return { x: 0, y: 0 }
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
   },
   mode: 'history'
 })
